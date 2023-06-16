974c2c379609839711d69fe3a1e061a4823c746b
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-06-16 23:20:42 +0300

    DI для Infrastructure

---
+++ Hotel.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,36 @@
+﻿using Hotel.App.Common.Interfaces;
+using Hotel.Infrastructure.Persistence;
+using Hotel.Infrastructure.Persistence.Repositories;
+using Hotel.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Hotel.Infrastructure;
+
+public static class DependencyInjection
+{
+	public static IServiceCollection AddInfrastructure(
+		this IServiceCollection services,
+		ConfigurationManager configuration)
+	{
+		services.AddScoped<IBookingRepository, BookingRepository>();
+		services.AddScoped<ICategoryRoomRepository, CategoryRoomRepository>();
+		services.AddScoped<IClientRepository, ClientRepository>();
+		services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+		services.AddScoped<IJobtitleRepository, JobtitleRepository>();
+		services.AddScoped<IRoomConditionRepository, RoomConditionRepository>();
+		services.AddScoped<IRoomRepository, RoomRepository>();
+		services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+
+		services.AddDbContext<ApplicationDbContext>(options =>
+		{
+			options.UseSqlServer(configuration.GetConnectionString("SqlServer"));
+		});
+
+
+		return services;
+	}
+}
