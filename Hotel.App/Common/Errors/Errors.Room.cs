ef4d4e087a8dae57f82328262eaefa3bd1375431
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-06-17 23:12:44 +0300

    Валидация и ошибки

---
+++ Hotel.App/Common/Errors/Errors.Room.cs
@@ -0,0 +1,17 @@
+﻿using ErrorOr;
+
+namespace Hotel.App.Common.Errors;
+
+public static partial class Errors
+{
+	public static class Room
+	{
+		public static Error InvalidId => Error.Validation(
+			code: "Room.InvalidId",
+			description: "Id номера не найден.");
+
+		public static Error NotFound => Error.Validation(
+			code: "Room.NotFound",
+			description: "Номер не найден.");
+	}
+}
