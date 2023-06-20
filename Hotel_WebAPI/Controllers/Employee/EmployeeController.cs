6a674328018d397f401f890c01b87e7fd1246b59
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-06-20 17:48:59 +0300

    Метод контроллера для информации о сотруднике

---
+++ Hotel_WebAPI/Controllers/Employee/EmployeeController.cs
@@ -0,0 +1,30 @@
+﻿using Hotel.App.Employee.Queries;
+using Hotel.Contracts.Room.Get;
+using MapsterMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using static Hotel.App.Common.Errors.Errors;
+
+namespace Hotel.API.Controllers.Employee;
+
+[ApiController]
+[Route("api/employee")]
+public class EmployeeController : ControllerBase
+{
+	private readonly ISender _mediator;
+	private readonly IMapper _mapper;
+
+
+	[HttpGet("{employeeId}")]
+	public async Task<IActionResult> GetDetailsOrder(string employeeId)
+	{
+		var query = new GetEmployeeDetailsQuery(employeeId);
+
+		var employeeResult = await _mediator.Send(query);
+
+		return employeeResult.Match(
+			employee => Ok(_mapper.Map<EmployeeResponse>(employee)),
+			errors => Problem("Ошибка")
+		);
+	}
+}
