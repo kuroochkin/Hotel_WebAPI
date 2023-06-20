5c83a51f720ac2fb83f63cb800bae0978fccccf7
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-06-20 17:43:31 +0300

    Метод поиска сотрудника по Id

--- Hotel.App/Common/Interfaces/IEmployeeRepository.cs
+++ Hotel.App/Common/Interfaces/IEmployeeRepository.cs
@@ -4,4 +4,5 @@ namespace Hotel.App.Common.Interfaces;
 
 public interface IEmployeeRepository : IGenericRepository<EmployeeEntity>
 {
+	Task<EmployeeEntity?> FindEmployeeWithJobtitle(Guid id);
 }
--- Hotel.Infrastructure/Persistence/Repositories/EmployeeRepository.cs
+++ Hotel.Infrastructure/Persistence/Repositories/EmployeeRepository.cs
@@ -2,6 +2,7 @@
 using Hotel.Infrastructure.Persistence.Repositories;
 using Hotel.Infrastructure.Persistence;
 using Hotel.Domain.Employee;
+using Microsoft.EntityFrameworkCore;
 
 namespace Hotel.Infrastructure.Repositories;
 
@@ -9,5 +10,12 @@ public class EmployeeRepository : GenericRepository<EmployeeEntity>, IEmployeeRe
 {
 	public EmployeeRepository(ApplicationDbContext context) : base(context)
 	{
+	}
+
+	public async Task<EmployeeEntity?> FindEmployeeWithJobtitle(Guid id)
+	{
+		return await _context.Employees
+			.Include(employee => employee.JobTitle)
+			.FirstOrDefaultAsync(employee => employee.Id == id);
 	}
 }
