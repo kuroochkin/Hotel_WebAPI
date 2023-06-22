85a772f2332dd42db038d014819e4ca7113de416
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-06-22 18:46:33 +0300

    Запрос информации о сотруднике по Id

--- Hotel.App/Employee/Queries/GetEmployeeDetails/GetEmployeeDetailsQuery.cs
+++ Hotel.App/Employee/Queries/GetEmployeeDetails/GetEmployeeDetailsQuery.cs
@@ -2,7 +2,7 @@
 using Hotel.App.Room.Vm;
 using MediatR;
 
-namespace Hotel.App.Employee.Queries;
+namespace Hotel.App.Employee.Queries.GetEmployeeDetails;
 
 public record GetEmployeeDetailsQuery(string EmployeeId) : IRequest<ErrorOr<EmployeeVm>>
 {
