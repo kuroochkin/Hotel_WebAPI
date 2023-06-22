60af8a40670717ee4ec9cfe4bee224d29517d2ff
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-06-22 18:47:29 +0300

    Методы контроллера Employee

---
+++ Hotel.Contracts/Employee/Requests/CreateEmployeeRequest.cs
@@ -0,0 +1,14 @@
+﻿using Hotel.Contracts.Room.Get;
+
+namespace Hotel.Contracts.Employee.Requests;
+
+public record CreateEmployeeRequest(
+	string LastName,
+	string FirstName,
+	string Patronymic,
+	DateTime Birthday,
+	string Education,
+	string JobtitleId,
+	decimal Salary
+	);
+
--- Hotel_WebAPI/Controllers/Employee/EmployeeController.cs
+++ Hotel_WebAPI/Controllers/Employee/EmployeeController.cs
@@ -1,9 +1,13 @@
-﻿using Hotel.App.Employee.Queries;
+﻿using Hotel.App.Employee.Commands.CreateEmployee;
+using Hotel.App.Employee.Queries.GetEmployeeDetails;
+using Hotel.App.Room.Commands.CreateBooking;
+using Hotel.Contracts.Employee.Requests;
 using Hotel.Contracts.Room.Get;
+using Hotel.Contracts.Room.Requests;
 using MapsterMapper;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using static Hotel.App.Common.Errors.Errors;
+
 
 namespace Hotel.API.Controllers.Employee;
 
@@ -14,6 +18,11 @@ public class EmployeeController : ControllerBase
 	private readonly ISender _mediator;
 	private readonly IMapper _mapper;
 
+	public EmployeeController(ISender mediator, IMapper mapper)
+	{
+		_mediator = mediator;
+		_mapper = mapper;
+	}
 
 	[HttpGet("{employeeId}")]
 	public async Task<IActionResult> GetDetailsOrder(string employeeId)
@@ -26,5 +35,18 @@ public class EmployeeController : ControllerBase
 			employee => Ok(_mapper.Map<EmployeeResponse>(employee)),
 			errors => Problem("Ошибка")
 		);
+	}
+
+	[HttpPost("create")]
+	public async Task<IActionResult> CreateEmployee(CreateEmployeeRequest request)
+	{
+		var command = _mapper.Map<CreateEmployeeCommand>(request);
+
+		var result = await _mediator.Send(command);
+
+		return result.Match(
+			employeeResult => Ok(result.Value),
+			errors => Problem("Ошибка")
+			);
 	}
 }
