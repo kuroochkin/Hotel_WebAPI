44f324368faecdd4b396d7962aca95ac465fc8ba
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-06-25 01:50:59 +0300

    Поиск номер по количеству комнат

---
+++ Hotel.App/Room/Queries/GetRoomsByCountSuite/GetRoomsByCountSuiteQuery.cs
@@ -0,0 +1,9 @@
+﻿using ErrorOr;
+using Hotel.App.Room.Vm;
+using MediatR;
+
+namespace Hotel.App.Room.Queries.GetRoomsByCountSuite;
+
+public record GetRoomsByCountSuiteQuery(int QuantitySuite) : IRequest<ErrorOr<RoomsVm>>
+{
+}
