37fa92d1b55b1c4301ef78a23c239cb654bfffe3
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-07-25 12:21:50 +0300

    Спецификация для запросов

---
+++ Hotel.Infrastructure/Persistence/Specifications/Specification.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq.Expressions;
+
+namespace Hotel.Infrastructure.Persistence.Specifications;
+
+public abstract class Specification<TEntity>
+	where TEntity : class
+{
+	protected Specification(Expression<Func<TEntity, bool>>? criteria)
+	{
+		Criteria = criteria;
+	}
+
+	protected Specification()
+	{
+	}
+
+	public Expression<Func<TEntity, bool>>? Criteria { get; } 
+
+	public List<Expression<Func<TEntity, object>>> IncludeExpressions { get; } = new();
+
+	protected void AddInclude(Expression<Func<TEntity, object>> includeExpression) =>
+		IncludeExpressions.Add(includeExpression);
+}
---
+++ Hotel.Infrastructure/Persistence/Specifications/SpecificationEvaluator.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Hotel.Infrastructure.Persistence.Specifications;
+
+public static class SpecificationEvaluator
+{
+	public static IQueryable<TEntity> GetQuery<TEntity>(
+		IQueryable<TEntity> inputQueryable,
+		Specification<TEntity> specification)
+		where TEntity : class
+	{
+		IQueryable<TEntity> queryable = inputQueryable;
+
+		if(specification.Criteria is not null)
+		{
+			queryable = queryable.Where(specification.Criteria);
+		}
+
+		specification.IncludeExpressions.Aggregate(
+			queryable,
+			(current, IncludeExpression) =>
+				current.Include(IncludeExpression));
+
+		return queryable;
+	}
+}
