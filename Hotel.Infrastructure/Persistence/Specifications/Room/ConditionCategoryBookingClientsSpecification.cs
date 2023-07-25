4a9fdeebd54e69c04b8eb10e0f6e342b69004855
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-07-25 12:22:13 +0300

    Реализация спецификации запросов

---
+++ Hotel.Infrastructure/Persistence/Specifications/Room/ConditionCategoryBookingClientsSpecification.cs
@@ -0,0 +1,24 @@
+﻿using Hotel.Domain.Room;
+using System.Linq.Expressions;
+
+namespace Hotel.Infrastructure.Persistence.Specifications.Room;
+
+public class ConditionCategoryBookingClientsSpecification : Specification<RoomEntity>
+{
+	public ConditionCategoryBookingClientsSpecification(Guid id) 
+		: base(room => room.Id == id)
+	{
+		AddInclude(room => room.Category);
+		AddInclude(room => room.Condition);
+		AddInclude(room => room.Condition.Booking);
+		AddInclude(room => room.Condition.Booking.Clients);
+	}
+
+	public ConditionCategoryBookingClientsSpecification()
+	{
+		AddInclude(room => room.Category);
+		AddInclude(room => room.Condition);
+		AddInclude(room => room.Condition.Booking);
+		AddInclude(room => room.Condition.Booking.Clients);
+	}
+}
---
+++ Hotel.Infrastructure/Persistence/Specifications/Room/ConditionCategoryBookingSpecification.cs
@@ -0,0 +1,21 @@
+﻿using Hotel.Domain.Room;
+using System.Linq.Expressions;
+
+namespace Hotel.Infrastructure.Persistence.Specifications.Room;
+ 
+public class ConditionCategoryBookingSpecification : Specification<RoomEntity>
+{
+	public ConditionCategoryBookingSpecification(Expression<Func<RoomEntity, bool>>? criteria) : base(criteria)
+	{
+		AddInclude(room => room.Category);
+		AddInclude(room => room.Condition);
+		AddInclude(room => room.Condition.Booking);
+	}
+
+	public ConditionCategoryBookingSpecification()
+	{
+		AddInclude(room => room.Category);
+		AddInclude(room => room.Condition);
+		AddInclude(room => room.Condition.Booking);
+	}
+}
