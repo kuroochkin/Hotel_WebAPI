b656c19eaa7274d82553c234697ea9ebc96bd98a
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-07-26 18:37:44 +0300

    Новые обработки ошибок Errors

---
+++ Hotel.App/CategoryRoom/Commands/AddConvenience/AddConvenienceCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using ErrorOr;
+using MediatR;
+
+namespace Hotel.App.CategoryRoom.Commands.AddConvenience;
+
+public class AddConvenienceCommandHandler
+	: IRequestHandler<AddConvenienceCommand, ErrorOr<bool>>
+{
+	public async Task<ErrorOr<bool>> Handle(
+		AddConvenienceCommand request, 
+		CancellationToken cancellationToken)
+	{
+		if (!Guid.TryParse(request.CategoryId, out var categoryId))
+		{
+			return Errors.Jobtitle.InvalidId;
+		}
+
+		if (!Guid.TryParse(request.ConvenienceId, out var convenienceId))
+		{
+			return Errors.Jobtitle.InvalidId;
+		}
+	}
+}
---
+++ Hotel.App/Common/Errors/Errors.Category.cs
@@ -0,0 +1,17 @@
+﻿using ErrorOr;
+
+namespace Hotel.App.Common.Errors;
+
+public static partial class Errors
+{
+	public static class Category
+	{
+		public static Error InvalidId => Error.Validation(
+			code: "Category.InvalidId",
+			description: "Id категории номера не найден.");
+
+		public static Error NotFound => Error.Validation(
+			code: "Category.NotFound",
+			description: "Категория номера не найден.");
+	}
+}
